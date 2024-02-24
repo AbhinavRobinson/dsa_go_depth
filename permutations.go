641ce8bde83c014a57b16a88eb4467363919e90a
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-02-24 15:03:33 +0530

    V.E12 Permutations of a string

--- permutations.go
+++ permutations.go
@@ -2,13 +2,15 @@ package main
 
 import "fmt"
 
-func permutation(str string, prefix string) { // O(N^2*N!)
+// V.E12 A function that calculates all possible permutations of a string.
+
+func permutation(str string, prefix string) { // O(N^2 * N!)
 	if len(str) == 0 {
 		fmt.Println(prefix) // O(N)
 	} else {
 		for i := 0; i < len(str); i++ { // O(N)
 			rem := str[0:i] + str[i+1:]             // O(1)
-			permutation(rem, prefix+string(str[i])) // N! Roots, each cost N -> O(N*N!)
+			permutation(rem, prefix+string(str[i])) // N! Roots, each cost N -> O(N * N!)
 		}
 	}
 }
